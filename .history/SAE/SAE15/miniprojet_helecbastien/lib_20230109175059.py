7459b251d9eba0feae192acc49ec3f7439333cdc
Bastien-Helec <bastienhelec.pro1@gmail.com>
2023-01-09 18:15:32 +0100

    fh

---
+++ .history/SAE/SAE15/miniprojet_helecbastien/lib_20230109175059.py
@@ -0,0 +1,34 @@
+#module lib
+# Copyright © Helec Bastien 
+#Version 0.0.1
+#2023-01-09
+
+# Instant de la mesure (H)
+T=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+
+# Série 1 : températures ( °C )
+L1=[3,3,4,3,2,5,8,9,13,16,18,18,19,21,22,22,21,17,17,12,10,8,7,4]
+
+# Série 2 : températures ( °C )
+L2=[103,203,4,3,2,5,8,9,13,16,18,18,19,21,22,22,21,17,17,12,10,-92,-93,-96]
+
+def moyenne(L):
+    """ Calcule la moyenne des valeurs d'une liste"""
+    global somme, moyennes
+    somme = 0
+    moyennes = 0
+    for i in range(len(L)):
+        somme = somme + L[i]
+        moyennes=somme/len(L)
+    print(moyennes)
+    
+moyenne(L1)
+moyenne(L2)
+
+def variance(L): 
+    """ Calcule la variance des valeurs d'une liste"""
+    moyenne(L)
+    for i in range(len(L)):
+        variance = (L[i] - moyennes)**2
+        print(variance)
+        
---
+++ .history/SAE/SAE15/miniprojet_helecbastien/lib_20230109180910.py
@@ -0,0 +1,46 @@
+#module lib
+# Copyright © Helec Bastien 
+#Version 0.0.1
+#2023-01-09
+import math
+# Instant de la mesure (H)
+T=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+
+# Série 1 : températures ( °C )
+L1=[3,3,4,3,2,5,8,9,13,16,18,18,19,21,22,22,21,17,17,12,10,8,7,4]
+
+# Série 2 : températures ( °C )
+L2=[103,203,4,3,2,5,8,9,13,16,18,18,19,21,22,22,21,17,17,12,10,-92,-93,-96]
+
+def moyenne(L):
+    """ Calcule la moyenne des valeurs d'une liste"""
+    global somme, moyennes
+    somme = 0
+    moyennes = 0
+    for i in range(len(L)):
+        somme = somme + L[i]
+        moyennes=somme/len(L)
+    print(moyennes)
+
+
+def variance(L): 
+    """ Calcule la variance des valeurs d'une liste"""
+    moyenne(L)
+    global variances
+    lvar=[]
+    for i in range(len(L)):
+        variances = (L[i] - moyennes)**2
+        lvar.append(variances)
+        
+    variances = moyenne(lvar)
+    print(variances)
+
+
+def ecart_type(L):
+    """ Calcule l'écart type des valeurs d'une liste"""
+    variance(L)
+    ecart_type= math.sqrt(variances)
+    print(ecart_type)
+
+ecart_type(L1)
+ecart_type(L2)
