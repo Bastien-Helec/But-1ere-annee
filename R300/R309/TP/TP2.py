988b33c16a1aeba62c31c5c6e6c988722ff227e7
Bastien-Helec <bastienhelec.pro1@gmail.com>
2023-11-08 09:22:12 +0100

    TP2 R309

---
+++ R300/R309/TP/TP2.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+#Exercie 1 
+
+#1.1 
+
+# Ct=Téléphone
+# CO=Ordinateur
+# S= Switch
+# R= Routeur
+
+# Realisation d'une page avec plusieurs fenetres permettant de faire des schémas réseaux
+Frame= Tk()
+Frame.title("TP2")
+Frame.geometry("800x600")
+Frame.minsize(480,360)
+Frame.config(background='#41B77F')
+Frame.mainloop()
