2e6b4d132365c02a24bec3c73f065a10c948cefd
Bastien-Helec <bastienhelec.pro1@gmail.com>
2023-11-14 16:30:00 +0100

    TP2.py

--- R300/R309/TP/TP2.py
+++ R300/R309/TP/TP2.py
@@ -23,26 +23,6 @@ right_pane = Frame(my_paned , background="beige", width=200, height=200)
 my_paned.add(left_pane)
 my_paned.add(right_pane)
 
-# Drag and drop des images et initialisation des images
-
-# client= ImageTk.PhotoImage(Image.open("client2.png")) 
-# router= ImageTk.PhotoImage(Image.open("router.png"))
-# switch= ImageTk.PhotoImage(Image.open("switch2.png"))
-
-
-
-# labelclient = Label(left_pane, image=client)
-# labelrouter= Label(left_pane, image=router)
-# labelswitch= Label(left_pane, image=switch)
-
-# labelclient.config(width=200,height=200)
-# labelrouter.config(width=50,height=50)
-# labelswitch.config(width=50,height=50)
-
-
-# labelclient.pack() 
-# labelrouter.pack()
-# labelswitch.pack()
 
 client=Label(left_pane, text="Client")
 router=Label(left_pane, text="Router")
@@ -51,12 +31,18 @@ client2=Label(right_pane, text="Client")
 router2=Label(right_pane, text="Router")
 switch2=Label(right_pane, text="Switch")
 
+client.tag="client"
+router.tag="router"
+switch.tag="switch"
+client2.tag="client2"
+router2.tag="router2"
+switch2.tag="switch2"
+
 
 client.grid(row=0,column=1)
 router.grid(row=1,column=1)
 switch.grid(row=2,column=1)
 
-#when client router or switch is click then create a new label with the same text and place it in the right pane and make it draggable on keep press
 
 def anywhere_object_deplacement(event): 
     event.widget.place(x=event.x, y=event.y)
@@ -79,7 +65,7 @@ def move_switch(event):
 
 def delete_object():
     print("delete")
-    destroy()
+    
 
 def properties_object():
     print("properties")
@@ -98,7 +84,7 @@ menu.add_command(label="Properties", command=properties_object)
 
 def menu_objects(event):
     try :
-
+        print(widget.item(curItem)["tags"])
         menu.tk_popup(event.x_root, event.y_root)
     finally:
         menu.grab_release()
@@ -117,6 +103,8 @@ switch.bind('<s>', move_switch)
 client2.bind("<Button-3>", menu_objects)
 router2.bind("<Button-3>", menu_objects)
 switch2.bind("<Button-3>", menu_objects)
+
+
 
 
 root.mainloop()
